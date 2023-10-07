c4f85f6f0b321855849d3b244a821c56eaf3a9e9
JoaoIto <joaovictorpfr@gmail.com>
2023-10-07 02:01:36 -0300

    add method without testing

--- src/test/java/test/CadastroResourceTest.java
+++ src/test/java/test/CadastroResourceTest.java
@@ -1,9 +1,11 @@
 package test;
 
 import dto.CadastroDTO;
+import dto.CadastroResponseDTO;
 import dto.LoginDTO;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import jakarta.inject.Inject;
 import org.junit.Test;
 import service.CadastroService;
@@ -11,6 +13,8 @@ import service.CadastroService;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class CadastroResourceTest {
@@ -55,5 +59,70 @@ public class CadastroResourceTest {
                 .when().get("/cadastro")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void testFindById() {
+        // Insira um cadastro de teste antes de buscar pelo ID
+        CadastroResponseDTO cadastroResponseDTO = cadastroService.insert(new CadastroDTO("Nome Teste", "Email Teste", "Nickname Teste", new LoginDTO("Senha Teste")));
+        Long id = cadastroResponseDTO.id();
+
+        given()
+                .when()
+                .get("/cadastro/" + id)
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(id));
+    }
+
+    @Test
+    public void testFindByNick() {
+        // Insira um cadastro de teste antes de buscar pelo nickname
+        CadastroResponseDTO cadastroResponseDTO = cadastroService.insert(new CadastroDTO("Nome Teste", "Email Teste", "Nickname Teste", new LoginDTO("Senha Teste")));
+        String nickname = "Nickname Teste";
+
+        given()
+                .when()
+                .get("/cadastro/search/nickname/" + nickname)
+                .then()
+                .statusCode(200)
+                .body("nickname[0]", equalTo(nickname));
+    }
+
+    @Test
+    public void testUpdate() {
+        // Insira um cadastro de teste antes de atualizar
+        CadastroResponseDTO cadastroResponseDTO = cadastroService.insert(new CadastroDTO("Nome Teste", "Email Teste", "Nickname Teste", new LoginDTO("Senha Teste")));
+        Long id = cadastroResponseDTO.id();
+
+        CadastroDTO updatedDTO = new CadastroDTO("Nome Atualizado", "Email Atualizado", "Nickname Atualizado", new LoginDTO("Senha Atualizada"));
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(updatedDTO)
+                .when()
+                .put("/cadastro/" + id)
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        CadastroResponseDTO updatedResponseDTO = response.as(CadastroResponseDTO.class);
+        assertEquals("Nome Atualizado", updatedResponseDTO.nome());
+        assertEquals("Email Atualizado", updatedResponseDTO.email());
+        assertEquals("Nickname Atualizado", updatedResponseDTO.nickname());
+    }
+
+    @Test
+    public void testDelete() {
+        // Insira um cadastro de teste antes de deletar
+        CadastroResponseDTO cadastroResponseDTO = cadastroService.insert(new CadastroDTO("Nome Teste", "Email Teste", "Nickname Teste", new LoginDTO("Senha Teste")));
+        Long id = cadastroResponseDTO.id();
+
+        given()
+                .when()
+                .delete("/cadastro/" + id)
+                .then()
+                .statusCode(204);
     }
 }
