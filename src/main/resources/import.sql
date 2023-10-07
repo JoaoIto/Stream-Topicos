f19eea7c74a4b0d2b44d5fe71c3ed8a7b156b47a
JoaoIto <joaovictorpfr@gmail.com>
2023-10-07 20:05:15 -0300

    add insert sql test in database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,11 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+INSERT INTO public.login (senha) VALUES ('senha1');
+INSERT INTO public.login (senha) VALUES ('senha2');
+
+INSERT INTO public.stream (custostream, nome, nomeusuario) VALUES (10.0, 'Stream1', 'Usuario1');
+INSERT INTO public.stream (custostream, nome, nomeusuario) VALUES (15.0, 'Stream2', 'Usuario2');
+
+INSERT INTO public.cadastro (id_cadastro, nickname, nome, email) VALUES (1, 'Nickname1', 'name', 'email1@example.com');
