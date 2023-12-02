9766f0f835b36a9d091ef3477cbdbdb455d6fd24
JoaoIto <joaovictorpfr@gmail.com>
2023-12-02 04:36:54 -0300

    adicionando status em response

--- src/main/java/br/unitins/topicos1/dto/SolicitacaoResponseDTO.java
+++ src/main/java/br/unitins/topicos1/dto/SolicitacaoResponseDTO.java
@@ -1,17 +1,20 @@
 package br.unitins.topicos1.dto;
 
 import br.unitins.topicos1.model.Solicitacao;
+import br.unitins.topicos1.model.StatusSolicitacao;
 
 public record SolicitacaoResponseDTO(
         Long id,
         DuoResponseDTO duo,
-        Float valorTotal
+        Float valorTotal,
+        StatusSolicitacao status
 ) {
     public static SolicitacaoResponseDTO valueOf(Solicitacao solicitacao) {
         return new SolicitacaoResponseDTO(
                 solicitacao.getId(),
                 DuoResponseDTO.valueOf(solicitacao.getDuo()),
-                solicitacao.getDuo().calcularCustoTotal()
+                solicitacao.getDuo().calcularCustoTotal(),
+                solicitacao.getStatus()
         );
     }
 }
