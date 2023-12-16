22454a91f04982f48475fa1aec5b1ce08e3d186b
JoaoIto <joaovictorpfr@gmail.com>
2023-12-16 05:51:22 -0300

    mensagem de erro de login

--- src/main/java/br/unitins/topicos1/dto/Login/LoginDTO.java
+++ src/main/java/br/unitins/topicos1/dto/Login/LoginDTO.java
@@ -3,9 +3,9 @@ package br.unitins.topicos1.dto.Login;
 import jakarta.validation.constraints.NotEmpty;
 
 public record LoginDTO (
-    @NotEmpty(message = "O campo nome não pode ser nulo.")
+    @NotEmpty(message = "O campo login não pode ser nulo.")
     String login,
-    @NotEmpty(message = "O campo nome não pode ser nulo.")
+    @NotEmpty(message = "O campo senha não pode ser nulo.")
     String senha
 ) {
 
